d39971260952c611a6164320c7a2a8d18dbf6023
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-20 23:52:57 +0900

    feat: AfterChat

---
+++ DatingSimulation/DatingSimulation/Source/Feature/Home/AfterChat.swift
@@ -0,0 +1,17 @@
+//
+//  AfterChat.swift
+//  DatingSimulation
+//
+//  Created by 최시훈 on 4/20/24.
+//
+
+import Foundation
+
+struct AfterChat: Identifiable {
+    var id = UUID()
+    var profileImage: String
+    var name: String
+    var mbti: String
+    var some: String
+    var relationship: String
+}
