c9cb33351a6aa8c4b0f01d5827f9e2d819a054d5
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-20 23:54:13 +0900

    feat: FlashView

---
+++ DatingSimulation/DatingSimulation/Source/Feature/Flash/FlashView.swift
@@ -0,0 +1,42 @@
+//
+//  FlashView.swift
+//  DatingSimulation
+//
+//  Created by 최시훈 on 4/20/24.
+//
+
+import SwiftUI
+import FlowKit
+
+struct FlashView: View {
+    @StateObject var viewModel = FlashViewModel()
+    @Flow var flow
+
+    var body: some View {
+        VStack {
+            
+            Spacer()
+            
+            Button {
+                flow.push(SetNameView())
+            } label: {
+                Text("대화 시작하기 \(Image(systemName: "arrow.right"))")
+                    .font(.suit())
+                    .foregroundStyle(.white)
+                    .frame(width: 328, height: 60)
+                    .background(.brand)
+                    .clipShape(
+                        RoundedRectangle(
+                            cornerRadius: 8
+                        )
+                    )
+            }
+            .padding(.bottom, 20)
+            .buttonStyle(CustomOreangeButtonStyle())
+        }
+    }
+}
+
+#Preview {
+    FlashView()
+}
