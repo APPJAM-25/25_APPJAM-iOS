2938efcac2b6d40f32ecf0d52ac69e097fc0e3af
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-20 16:01:52 +0900

    init

---
+++ DatingSimulation/DatingSimulation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DatingSimulation
+//
+//  Created by 최시훈 on 4/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DatingSimulation/DatingSimulation/DatingSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingSimulationApp.swift
+//  DatingSimulation
+//
+//  Created by 최시훈 on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct DatingSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
