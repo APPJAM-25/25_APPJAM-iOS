892bd09411ce19c36bc6a8cbeb0ce846f5d5bede
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-20 23:53:31 +0900

    feat: SetAgeView

---
+++ DatingSimulation/DatingSimulation/Source/Feature/Flash/SetAge/SetAgeView.swift
@@ -0,0 +1,41 @@
+//
+//  SetAgeView.swift
+//  DatingSimulation
+//
+//  Created by 최시훈 on 4/20/24.
+//
+
+import SwiftUI
+import FlowKit
+
+struct SetAgeView: View {
+    @Flow var flow
+    @StateObject var viewModel = SetAgeViewModel()
+    
+    var body: some View {
+        CustomTextField(
+            text: "나이",
+            input: $viewModel.age,
+            placeholder: "나이를 입력 해주세요"
+        )
+        .keyboardType(.numberPad)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .padding(.top, 85)
+        
+        
+        Spacer()
+        
+        Button {
+            flow.push(SetGenderView())
+        } label: {
+            Text("다음으로")
+        }
+        .padding(.bottom, 20)
+        .buttonStyle(CustomOreangeButtonStyle())
+    }
+    
+}
+
+#Preview {
+    SetAgeView()
+}
